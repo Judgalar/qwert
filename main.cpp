1a5a61437cf994a6c17d4066b523492d53783d90
Poliest <93813028+Poliest@users.noreply.github.com>
2021-12-20 09:06:03 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int numero1 , numero2;
+    int suma;
+    
+    cout<<"Introduce primer numero: ";
+    cin >> numero1;
+    cout<<"Introduce segundo numero: ";
+    cin >> numero2;
+
+    suma=numero1+numero2;
+
+    cout<<endl;
+    cout<<"La suma de "<<numero1<<" y "<<numero2<<" es "<<suma<<endl;
+
+}
